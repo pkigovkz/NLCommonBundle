aab49819c88712864e8e3560bedc4e24b7427364
aslan <me@github>
2018-06-05 18:18:08 +0600

    the rest of the source

--- src/main/java/kz/gov/pki/knca/types/ResponseMessage.java
+++ src/main/java/kz/gov/pki/knca/types/ResponseMessage.java
@@ -7,6 +7,11 @@ import lombok.Data;
  */
 @Data
 public class ResponseMessage<T> {
+
+    public ResponseMessage(String code) {
+        this.code = code;
+    }
+
     private String code;
     private String message;
     private T responseObject;
